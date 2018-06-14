d184f96306959ffc7cdecf560eb79c5cf744d1ea
nf <nflower@adaptiveplanningandexecution.com>
2018-06-14 06:07:40 -0700

    viewmodels

---
+++ OAuthTutorial/Models/OAuthClientViewModels/CreateClientViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OAuthTutorial.Models.OAuthClientViewModels {
+    public class CreateClientViewModel {
+        [Required]
+        [MinLength(2)]
+        [MaxLength(100)]
+        public string ClientName { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        [MaxLength(500)]
+        public string ClientDescription { get; set; }
+    }
+}
---
+++ OAuthTutorial/Models/OAuthClientViewModels/EditClientViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OAuthTutorial.Models.OAuthClientViewModels {
+    public class EditClientViewModel {
+        [Required]
+        [MinLength(1)]
+        [MaxLength(500)]
+        public string ClientDescription { get; set; }
+
+        public string ClientName { get; internal set; }
+
+        public string ClientId { get; internal set; }
+
+        public string ClientSecret { get; internal set; }
+
+        public string[] RedirectUris { get; set; } = new string[0];
+    }
+}

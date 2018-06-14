536c002471327fee939ecbeb3a12290af87c9575
nf <nflower@adaptiveplanningandexecution.com>
2018-06-13 18:10:48 -0700

    Changed launch settings; stubbed API Controller

---
+++ OAuthTutorial/Controllers/APIController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OAuthTutorial.Data;
+using OAuthTutorial.Models;
+using System.Threading.Tasks;
+
+namespace OAuthTutorial.Controllers {
+
+    [Route("/api/v1/")]
+    public class APIController : Controller {
+
+        private readonly ILogger _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public APIController(ILogger<ManageController> logger, ApplicationDbContext context, UserManager<ApplicationUser> userManager) {
+            _logger = logger;
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // Unauthenticated Methods - available to the public
+        [HttpGet("hello")]
+        public IActionResult Hello() {
+            return Ok("Hello");
+        }
+
+        // Authenticated Methdos - only available to those with a valid Access Token
+        // Unscoped Methods - Authenticated methods that do not require any specific Scope
+        [HttpGet("clientcount")]
+        public async Task<IActionResult> ClientCount() {
+            return Ok("Client Count Get Request was successful but this endpoint is not yet implemented");
+        }
+
+        // Scoped Methods - Authenticated methods that require certain scopes
+        [HttpGet("birthdate")]
+        public IActionResult GetBirthdate() {
+            return Ok("Birthdate Get Request was successful but this endpoint is not yet implemented");
+        }
+
+        [HttpGet("email")]
+        public async Task<IActionResult> GetEmail() {
+            return Ok("Email Get Request was successful but this endpoint is not yet implemented");
+        }
+
+        [HttpPut("birthdate")]
+        public IActionResult ChangeBirthdate(string birthdate) {
+            return Ok("Birthdate Put successful but this endpoint is not yet implemented");
+        }
+
+        [HttpPut("email")]
+        public async Task<IActionResult> ChangeEmail(string email) {
+            return Ok("Email Put request received, but function is not yet implemented");
+        }
+
+        // Dynamic Scope Methods - Authenticated methods that return additional information the more scopes are supplied
+        [HttpGet("me")]
+        public async Task<IActionResult> Me() {
+            return Ok("User Profile Get request received, but function is not yet implemented");
+        }
+    }
+}

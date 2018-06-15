e422f56724d0480b785787349778a38b859e6aad
nf <nflower@adaptiveplanningandexecution.com>
2018-06-15 11:24:48 -0700

    started on view controllers

---
+++ OAuthTutorial/Controllers/AuthorizeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OAuthTutorial.Data;
+using OAuthTutorial.Models;
+
+namespace OAuthTutorial.Controllers {
+
+    [Route("/authorize/")]
+    public class AuthorizeController : Controller {
+
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AuthorizeController(ApplicationDbContext context, UserManager<ApplicationUser> userManager) {
+            _context = context;
+            _userManager = userManager;
+        }
+
+
+        public async Task<IActionResult> Index() {
+            return Ok();
+        }
+
+        [HttpPost("deny")]
+        public async Task<IActionResult> Deny() {
+            return LocalRedirect("/");
+        }
+
+        [HttpPost("accept")]
+        public async Task<IActionResult> Accept() {
+            // TODO this will be a big method, we'll address it further down below.
+            return Ok();
+        }
+
+    }
+}
---
+++ OAuthTutorial/Models/AuthorizeViewModels/AuthorizeViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OAuthTutorial.Models.AuthorizeViewModels
+{
+    public class AuthorizeViewModel {
+
+        public string ClientName { get; internal set; }
+
+        public string ClientId { get; internal set; }
+
+        public string ClientDescription { get; internal set; }
+
+        public string ResponseType { get; internal set; }
+
+        public string RedirectUri { get; internal set; }
+
+        public string[] Scopes { get; internal set; } = new string[0];
+
+        public string State { get; internal set; }
+
+    }
+}

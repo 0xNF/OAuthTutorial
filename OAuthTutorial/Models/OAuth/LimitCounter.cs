3c1d783ad5f5012f5cdddd03066e366bf6d92631
nf <nflower@adaptiveplanningandexecution.com>
2018-06-18 13:44:30 -0700

    Rate Limti attribute added

---
+++ OAuthTutorial/Models/OAuth/LimitCounter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OAuthTutorial.Models.OAuth {
+    public struct LimitCounter {
+        public readonly DateTimeOffset FirstCallTimestamp;
+        public readonly TimeSpan Window;
+        public readonly int CallCount;
+        public readonly int? Limit;
+
+        public LimitCounter(DateTimeOffset firstCallTimestamp, TimeSpan window, int callCount, int? limit) {
+            FirstCallTimestamp = firstCallTimestamp;
+            CallCount = callCount;
+            Limit = limit;
+            Window = window;
+        }
+    }
+}
